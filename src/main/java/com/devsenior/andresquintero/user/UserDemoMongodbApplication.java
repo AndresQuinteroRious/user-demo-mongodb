67be813a743d89bce90ea2635eaf1b2df91c718d
Andrés Mauricio Quintero Ríos <quintero2273@hotmail.com>
2026-01-11 07:11:36 -0500

    estructura inicial

---
+++ src/main/java/com/devsenior/andresquintero/user/UserDemoMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.devsenior.andresquintero.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDemoMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDemoMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsenior/andresquintero/user/UserDemoMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.andresquintero.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDemoMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
